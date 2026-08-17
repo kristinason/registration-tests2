754778163a558edc7cd77d993b11764474ae2b7f
kristinason <kristinason.sw@gmail.com>
2026-08-17 10:19:37 +0200

    Update Hooks.java

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,4 +1,25 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void beforeScenario() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1280,900");
+        driver = new ChromeDriver(options);
+    }
+
+    @After
+    public void afterScenario() {
+        if (driver != null) driver.quit();
+    }
 }
